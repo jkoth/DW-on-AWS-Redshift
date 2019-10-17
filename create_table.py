cc0bfe95a5eaf1ec36ff7d0dd6e1d1ddfefc2149
jkoth <kothari.jubin88@gmail.com>
2019-10-17 01:49:03 -0500

    feat: Define Python function drop_tables() to process DROP TABLE sql statements.
    Function accepts two params, connection and cursor to the AWS Redshift.
    List containing DROP TABLE queries are sourced from sql_queries.py file saved in same path.

---
+++ create_table.py
@@ -0,0 +1,23 @@
+import configparser               # Parse configuration file
+import psycopg2                   # PostgreSQL database adapter for the Python
+import sys                        # Used for exiting python script in case of error
+#from no_op_sql_queries import create_table_queries, drop_table_queries      #Non-Optimized DWH Tables
+from sql_queries import create_table_queries, drop_table_queries    # SQL query definitions
+
+"""
+Purpose:
+    Loops through drop_table_queries list and runs drop table queries
+    drop_table_queries are defined in sql_queries file
+Arg:
+    cur - Redshift connection cursor [Required]
+    conn - Redshift connection [Required]
+"""
+def drop_tables(cur, conn):
+    num_queries = len(drop_table_queries)
+    query_count = 0
+    for query in drop_table_queries:
+        query_count += 1
+        print("Running ", query_count, "/", num_queries, " drop table queries")
+        cur.execute(query)
+        conn.commit()
+
